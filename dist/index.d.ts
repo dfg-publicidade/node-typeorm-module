56d7f976e6d553d7a040c617287f715c8ffac956
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-03-20 09:43:43 -0300

    Correções e testes

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+import TypeOrmManager from './datasources/typeOrmManager';
+import JoinType from './enums/joinType';
+import DefaultService from './services/defaultService';
+export { TypeOrmManager, DefaultService, JoinType };
